c1be2668f39b99ed301d32e6d6077e0a44540703
Somanos Sar <somanos@drumee.com>
2026-05-23 15:29:01 +0700

    Cleaned node_modules

--- .dev-tools.rc/deploy.sh
+++
@@ -1,6 +0,0 @@
-# Change these variables accordingly to you configs
-#export DEST_HOST=production.host.example
-#export DEST_USER=yourusername
-#export DEST_DIR=/path/to/your/drumee/endpoint
-#export SRC_PATH=/path/to/your/drumee/server/source
-#export ENDPOINT=main
--- .dev-tools.rc/devel.sh
+++
@@ -1,4 +0,0 @@
-export ENDPOINT=""
-export DEST_HOST=drumee.in
-export DEST_USER=huan
-export DEST_DIR=/srv/drumee/static
